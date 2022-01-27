a4528e67b0a6f9a79e692c9bab7ca3ff6e16da40
Vladyslav <72090640+Kotliarenko02@users.noreply.github.com>
2022-01-27 18:51:38 +0200

    8.80/10 (previous run: 8.57/10, +0.23)

--- SW_Exam.py
+++ SW_Exam.py
@@ -8,19 +8,16 @@ NEG = "Negative gamma"
 INV = "Not valid input"
 
 def results(x):
-    str_num = ""
     print("Input: ",x)
     result = ""
     if (isinstance(x) == int or isinstance(x) == float):
         num = math.gamma(float(x))
-        str_num = str(num)
         if num> 0:
             result = POS
         else:
             result = NEG
     else:
         result = INV
-        str_num="Invalid input"
     print(result,"\n")
     return result
 
